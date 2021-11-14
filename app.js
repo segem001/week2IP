ad043c03d2ff1ff8812728d7604222385615e230
segem001 <kipdom001@gmail.com>
2021-11-14 22:01:45 +0300

    Declare variables maleNames and femaleNames

---
+++ app.js
@@ -0,0 +1,6 @@
+function feedback (){
+
+    let maleNames=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
+    let femaleNames=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
+    let Days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+    // Ghanian_Generator function
