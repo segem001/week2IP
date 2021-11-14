13d235f25858538ca83cdf3785a0c11b8159a181
segem001 <kipdom001@gmail.com>
2021-11-14 22:04:50 +0300

    Declare variables date_number,month_number and year_number

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ function feedback (){
     let femaleNames=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
     let Days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
     // Ghanian_Generator function
+
+    let date_number=document.getElementById('date');
+    let month_number=document.getElementById('month');
+    let year_number=document.getElementById('year'); 
+    
+    
