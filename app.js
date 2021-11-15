fa3fcd16fcbc3c5ef374c5c09e4109fef6909e14
segem001 <kipdom001@gmail.com>
2021-11-15 18:36:50 +0300

    modify the javascript language

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ function feedback (){
 
     let date_number=document.getElementById('date'); 
     if (date_number.value<=0 || date_number.value>31){
-      alert("Invalid Date Number")
+      alert("Invalid Date Number");return false
     }
     let month_number=document.getElementById('month');
     if (month_number.value<=0 || month_number.value>12){
-        alert("Invalid Month Number")
+        alert("Invalid Month Number");return false
       }
     let year_number=document.getElementById('year'); 
     
@@ -20,53 +20,29 @@ function feedback (){
      let YY=parseInt(year_number.value.slice(2,4));
      let MM=parseInt(month_number.value);
      let DD=parseInt(date_number.value);
-     // alert(date_number.value)
- 
-     // let weekDay = Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7)
-    let weekDay = (( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7).toFixed()
- 
- 
-     // alert(weekDay)
-     
-     //radio values
+
+     var birth_date=(MM+'/'+DD+'/'+CC+YY)
+     date_number.value=""
+     month_number.value=""
+     year_number.value=""
+    //  alert (birth_date)
+    var get_time=new Date(birth_date).getDay();
+
      let maleRadio=document.getElementById("males");
     let femaleRadio=document.getElementById("females");
     
-    //looping 
-    
-    let outputs="";
-    
-    if(weekDay==0){
-    
-        outputs=6;
-    
-    }else{
-    
-        outputs=weekDay-1;
-    
-    }
-    //feedback function
-    
-   
-        // formDisplay.style.display="block";
-        // alert(YY)
-        // alert(year_number.value)
         let userdetails=document.getElementById("results");
      
     if(maleRadio.checked){
 
-        userdetails.value= `Your Akan name is ${femaleNames[outputs]} and was born on ${Days[outputs]}`;
-
-        // userdetails.innerHTML= "Your Akan name is "+maleNames[outputs] + "and was born on" +;
-        // userdetails.innerHTML= "Your Akan name is";
-        // userdetails.value= `Your Akan name is ${maleNames[outputs]} `;
-        // userdetails.style.display="block";
-    
+        userdetails.value= `Your Akan name is ${femaleNames[get_time]} and was born on ${Days[get_time]}`;
+          return false;
+        
     }else if(femaleRadio.checked){
-        userdetails.value= `Your Akan name is ${maleNames[outputs]} and was born on ${Days[outputs]}`;
-    userdetails.style.display="block";
+        userdetails.value= `Your Akan name is ${maleNames[get_time]} and was born on ${Days[get_time]}`;
+    userdetails.style.display="block";return false
     }else if(!(maleRadio.checked || femaleRadio.checked)){
-        alert("Choose your gender")
+        alert("Choose your gender");return false
     }
        
     
